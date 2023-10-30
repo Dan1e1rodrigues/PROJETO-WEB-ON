ad7ec3a0ac4934e4018b287393db5c1ec4fb6ff9
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-10-30 16:17:14 -0300

    UP

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -7,11 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    // Salve a senha de forma segura no localStorage (não recomendado em um ambiente real)
+    // Salve o email e senha no localStorage (não recomendado em um ambiente real)
     localStorage.setItem('email', email);
     localStorage.setItem('senha', senha);
 
     alert('Cadastro bem-sucedido!');
-    window.location.href = '../HTML/ARQUIVO.HTML'; // Redirecione para a página de login
+    window.location.href = '../HTML/login.html'; // Redirecione para a página de login
   });
 });
+
--- JS/login.js
+++ JS/login.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.querySelector('form');
-  
-    loginForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-  
-      // Recupere a senha do localStorage
-      const savedPassword = localStorage.getItem('senha');
-  
-      // Verifique as credenciais
-      if (username === 'seuUsuario' && password === savedPassword) {
-        alert('Login bem-sucedido! Redirecionando para a página principal.');
-        window.location.href = 'pagina_principal.html'; // Redirecione para a página principal
-      } else {
-        alert('Login falhou. Verifique suas credenciais.');
-      }
-    });
+  const loginForm = document.querySelector('form');
+
+  loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Recupere o email e senha do localStorage
+    const savedEmail = localStorage.getItem('email');
+    const savedPassword = localStorage.getItem('senha');
+
+    // Verifique as credenciais
+    if (username === savedEmail && password === savedPassword) {
+      alert('Login bem-sucedido! Redirecionando para a página principal.');
+      window.location.href = '../HTML/ARQUIVO.HTML'; // Redirecione para minha home
+    } else {
+      alert('Login falhou. Verifique suas credenciais.');
+    }
   });
+});
+
   
   
   
