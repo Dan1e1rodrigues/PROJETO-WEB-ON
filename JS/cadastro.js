ffa82cc48bbb585dc34d5cff7363c9323c04ed1d
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2023-10-30 21:25:16 -0300

    UP

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -1,14 +1,21 @@
+//Função evento DOM
 document.addEventListener('DOMContentLoaded', function() {
+
+  //Vai buscar as informações contidas dentro do form 
   const form = document.querySelector('form');
 
+  //Function evento de submissão   
   form.addEventListener('submit', function(event) {
+
     event.preventDefault();
 
     const email = document.getElementById('email').value;
+
     const senha = document.getElementById('senha').value;
 
     // Salve o email e senha no localStorage (não recomendado em um ambiente real)
     localStorage.setItem('email', email);
+
     localStorage.setItem('senha', senha);
 
     alert('Cadastro bem-sucedido!');
